604b39b6a64c5f4d0181cca616d3fded5adcbfc4
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-04 11:36:55 +0200

    reset button added (not working)

--- JS/mainjs.js
+++ JS/mainjs.js
@@ -1,12 +1,16 @@
 const btnConfirm = document.querySelector(".btn-confirm");
+const btnReset = document.querySelector(".btn-reset");
 
 const currentYear = new Date().getFullYear();
 
 const tripLengthInput = document.getElementById("trip-length");
 const yearOfBirthInput = document.getElementById("year-of-birth");
+const ticket = document.getElementById("finalData")
 
 
 btnConfirm.addEventListener("click", function getData() {
+  ticket.classList.replace("d-none", "d-block");
+
   const tripLength = tripLengthInput.value;
   const yearOfBirth = yearOfBirthInput.value;
 
@@ -25,4 +29,8 @@ btnConfirm.addEventListener("click", function getData() {
 
   document.getElementById('testAge').innerHTML = `You are ${userAge} years old`;
   document.getElementById('priceResult').innerHTML = `Your ${tripLength}km trip costs ${finalPrice}€`;
+})
+
+btnReset.addEventListener("click", function hide() {
+  ticket.classList.replace("d-block", "d-none");    //non funziona
 })
