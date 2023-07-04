86112c7b8f4d407df6418e6ad1e9ab0c42a0fb72
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-04 10:52:43 +0200

    fix generali

--- JS/mainjs.js
+++ JS/mainjs.js
@@ -1,29 +1,28 @@
-const btnConfirm = document.querySelector('btn.confirm');
-
-btnConfirm.addEventListener("Click", function getData() {
-  const tripLength = document.getElementById("trip-length").value;
-  const yearOfBirth = document.getElementById("year-of-birth").value;
-})
-//da inserire nella funzione stessa i richiami delle variabili
+const btnConfirm = document.querySelector(".btn-confirm");
 
 const currentYear = new Date().getFullYear();
-const userAge = currentYear - parseInt(yearOfBirth);
 
-document.getElementById('testAge').innerHTML = `You are ${userAge} years old`;
+const tripLengthInput = document.getElementById("trip-length");
+const yearOfBirthInput = document.getElementById("year-of-birth");
 
-let ticketPrice = 0.21 * parseInt(tripLength);
 
-let discount = 1;
+btnConfirm.addEventListener("click", function getData() {
+  const tripLength = tripLengthInput.value;
+  const yearOfBirth = yearOfBirthInput.value;
 
-if (userAge < 18) {
-  discount = 0.8;
-} else if (userAge > 65) {
-  discount = 0.6;
-}
+  let ticketPrice = 0.21 * parseInt(tripLength);
 
-const finalPrice = Math.round((ticketPrice *= discount) * 100) / 100;
-//alternativa
-//const finalPrice = (ticketPrice *= discount).toFixed(2);
-//il parseInt che wrappa tutto, non funziona. Da capire poi perchè
+  let discount = 1;
+  const userAge = currentYear - parseInt(yearOfBirth);
 
-document.getElementById('priceResult').innerHTML = `Your ${tripLength}km trip costs ${finalPrice}€`;
+  if (userAge < 18) {
+    discount = 0.8;
+  } else if (userAge > 65) {
+    discount = 0.6;
+  }
+
+  const finalPrice = Math.round((ticketPrice *= discount) * 100) / 100;
+
+  document.getElementById('testAge').innerHTML = `You are ${userAge} years old`;
+  document.getElementById('priceResult').innerHTML = `Your ${tripLength}km trip costs ${finalPrice}€`;
+})
